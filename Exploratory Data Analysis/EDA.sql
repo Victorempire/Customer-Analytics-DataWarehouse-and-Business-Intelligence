c385894f68ab079d83fa3e3a8ac91e387424f2eb
Victorempire12 <158476575+Victorempire@users.noreply.github.com>
2025-12-10 14:34:58 +0100

    Update EDA.sql

--- Exploratory Data Analysis/EDA.sql
+++ Exploratory Data Analysis/EDA.sql
@@ -1,1 +1,16 @@
+1. How have revenue, cost, and profit margins trended year-over-year?
+SELECT [Year],
+	   Total_Revenue,
+	   Operational_Cost,
+	   (Total_Revenue-Operational_Cost) AS Profit,
+	  FORMAT(ROUND(CAST((Total_Revenue-Operational_Cost) AS float)/Total_Revenue,3),'P') AS Profit_Margin
+FROM(
+SELECT  DATEPART(YEAR,transaction_date) AS [Year],
+		SUM(transaction_amount_ngn) AS Total_Revenue,
+	    SUM(cost_to_serve_ngn) AS Operational_Cost
+FROM Silver.crm_transaction
+GROUP BY DATEPART(YEAR,transaction_date)
+) T
+ORDER BY [Year] ASC;
+GO
 
